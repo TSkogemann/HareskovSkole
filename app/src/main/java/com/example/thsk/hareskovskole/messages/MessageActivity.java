69738406513cc3d9f1b74d2d6a787bd95d2c7220
thsk <thsk@nodes.dk>
2017-07-02 01:01:31 +0200

    start på msg xml

--- app/src/main/java/com/example/thsk/hareskovskole/messages/MessageActivity.java
+++ app/src/main/java/com/example/thsk/hareskovskole/messages/MessageActivity.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
+import com.example.thsk.hareskovskole.MenuActivity;
 import com.example.thsk.hareskovskole.R;
 
 /**
  * Created by thsk on 20/06/2017.
  */
 
-public class MessageActivity extends FragmentActivity {
+public class MessageActivity extends MenuActivity {
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
